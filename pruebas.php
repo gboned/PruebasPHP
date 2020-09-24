97d90b5fc7e9ac5ee0b72fa2ccaf80bb325e81d8
gboned <gboned@cifpfbmoll.eu>
2020-09-24 17:03:49 +0200

    Declarar e imprimir constantes

--- pruebas.php
+++ pruebas.php
@@ -18,3 +18,17 @@
     echo $numero . "\n";
 
     echo "HOLA MUNDO";
+
+    // Declarar una constante, define("parámetro 1: nombre de constante", "parámetro 2: texto para constante"):
+    # Constantes NO pueden cambiar nunca su valor en todo el Script.
+    define ('MI_CONSTANTE', 'CONSTANTE');
+
+    # Constante array
+    define ('MI_ARRAY', array("2", "3"));
+
+    // Asignar las constantes a variables:
+    $_miconstante = MI_CONSTANTE;
+    $_miarray = MI_ARRAY;
+
+    // Imprimir las constantes por pantalla.
+    echo "\t" . MI_CONSTANTE . "\t" . $_miarray . "\n";
