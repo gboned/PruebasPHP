7626670a932e743b1dd66a1ddb93cd5ed312f195
gboned <gboned@cifpfbmoll.eu>
2020-09-25 00:04:40 +0200

    Mover php y html a fichero nuevo

---
+++ Fundamentos/PHPyHTML.php
@@ -0,0 +1,17 @@
+<?php
+
+/*     Incluir PHP dentro de HTML. PHP se interpreta en el servidor.
+         <html>
+        <head>
+            <title></title>
+        </head>
+        <body>
+            <h2>Hola, soy un html</h2>
+            <?php
+                $variable1 = "25";
+                $variable2 = "5";
+
+                echo $variable1 * $variable2;
+            ?>
+        </body>
+        </html>  */
