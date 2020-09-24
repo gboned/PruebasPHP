bfed137b37e391209ab0a944848b12033ee9b28b
gboned <gboned@cifpfbmoll.eu>
2020-09-24 17:03:16 +0200

    Crear e imprimir variables

---
+++ pruebas.php
@@ -0,0 +1,20 @@
+<?php
+
+
+    // Esto es un comentario
+    # Esto también es un comentario
+    /** Y esto también es un comentario, esta vez
+     * en bloque.
+     */
+
+    // Declarar variables, añadiendo un '$' al principio, se devuelven
+    // también con echo.
+    $saludo = "Hola, ¿Cómo estás?";
+    $numero = 24;
+
+    // Para añadir un salto de línea, primero concatenamos las distintas
+    // variables con un '.', luego añadimos "\n".
+    echo $saludo . "\n";
+    echo $numero . "\n";
+
+    echo "HOLA MUNDO";
