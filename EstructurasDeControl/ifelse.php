9627c468c0198c2fb02e249aa6ec222d229d0765
gboned <gboned@cifpfbmoll.eu>
2020-09-25 13:17:33 +0200

    Añadir fichero estructuras de control y archivo if else

---
+++ EstructurasDeControl/ifelse.php
@@ -0,0 +1,14 @@
+<?php
+
+    $a = "5";
+    $b = "10";
+
+    if ($a == $b) {
+        echo "Los dos números son iguales";
+    }
+    elseif ($a > $b) {
+        echo "'a' es mayor que 'b'";
+    }
+    else {
+        echo "Los números son diferentes, y 'a' es menor que 'b'";
+    }
